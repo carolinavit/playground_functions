6c6de2644942d68ecd4c0c0d1b84ed055710452c
Vitória <vitcs11@gmail.com>
2022-08-27 01:03:40 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,24 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeros) {
+  if (numeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] < 0 || numeros[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let cont = 0;
+    for (let index2 = 0; index2 < numeros.length; index2 += 1) {
+      if (numeros[index] === numeros[index2]) {
+        cont += 1;
+      }
+    }
+    if (cont >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  return `(${numeros[0]}${numeros[1]}) ${numeros[2]}${numeros[3]}${numeros[4]}${numeros[5]}${numeros[6]}-${numeros[7]}${numeros[8]}${numeros[9]}${numeros[10]}`;
 }
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
